b986f04ca3663fb52d44b3e2951d9a7b67e0019a
jame-4xtreme <96515476+jame-4xtreme@users.noreply.github.com>
2022-01-17 16:11:18 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php
 
 /* validate verify token needed for setting up web hook */ 
-echo 'Facebook bot 0910 ****************************************************************************';
+echo 'Facebook bot 1611 ****************************************************************************';
 
 
 /* validate verify token needed for setting up web hook */ 
 
 if (isset($_GET['hub_verify_token'])) { 
-    if ($_GET['hub_verify_token'] === $access_token) {
+    if ($_GET['hub_verify_token'] === 'pop') {
         echo $_GET['hub_challenge'];
         return;
     } else {
